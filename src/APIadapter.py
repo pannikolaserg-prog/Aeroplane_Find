864874e7ff3845d070cbd13c5dcf84a864f55d27
Nikolay Panov <pannikolaserg@gmail.com>
2026-02-10 23:36:08 +0900

    add class get_coordinates, get_aeroplanes and plane

--- src/APIadapter.py
+++ src/APIadapter.py
@@ -7,15 +7,22 @@ import requests
 class AbstractAdapter(ABC):
 
     @abstractmethod
+    def get_coordinates(self, country):
+        pass
+
+    @abstractmethod
     def get_aeroplanes(self, country: str) -> None:
         pass
 
 class APIAdapter(AbstractAdapter):
+
     def __init__(self):
         self.openstreetmap_url = 'https://nominatim.openstreetmap.org/search'
-        self.opensky_url = 'https://opensky-network.org/api'
+        self.opensky_url = 'https://opensky-network.org/api/states/all'
         self.aeroplanes = None
-    def get_aeroplanes(self, country: str) -> None:
+
+    def get_coordinates(self, country):
+
         headers_nominatim = {
             'User-Agent': 'test-app/1.0',
         }
@@ -30,7 +37,34 @@ class APIAdapter(AbstractAdapter):
 
         data = response.json()
 
-        return json.dumps(data, indent=4)
+        # Получаем bounding box
+        geo_coordinates = data[0].get("boundingbox")
+
+        return geo_coordinates
+
+    def get_aeroplanes(self, coordinates: list) -> None:
+
+        params = {
+            'lamin': coordinates[0],
+            'lamax': coordinates[1],
+            'lomin': coordinates[2],
+            'lomax': coordinates[3],
+        }
+
+        response = requests.get(url=self.opensky_url, params=params)
+
+        self.aeroplanes = response.json()
 
 test = APIAdapter()
-print(test.get_aeroplanes("Russia"))
+coordinates = test.get_coordinates("Russia")
+test.get_aeroplanes(coordinates)
+print(test.aeroplanes)
+
+class Plane:
+
+    def __init__(self, country, callsiqn, speed, geo_altitude):
+        self.country = country
+        self.callsign = callsiqn
+        self.speed = speed
+        self.geo_altitude = geo_altitude
+
